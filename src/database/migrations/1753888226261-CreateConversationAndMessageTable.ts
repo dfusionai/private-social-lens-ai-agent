a7c99de3cca5207cfa03ab375edaaf48d7eeeacd
Nam Dang <namquoc.dev@gmail.com>
2025-07-30 22:12:42 +0700

    fix(db): migraion script

--- src/database/migrations/1753888226261-CreateConversationAndMessageTable.ts
+++ src/database/migrations/1753888226261-CreateConversationAndMessageTable.ts
@@ -1,16 +1,16 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateConversationAndMessageTable1752032831547
+export class CreateConversationAndMessageTable1753888226261
   implements MigrationInterface
 {
-  name = 'CreateConversationAndMessageTable1752032831547';
+  name = 'CreateConversationAndMessageTable1753888226261';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "conversation" ("title" character varying, "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" integer NOT NULL, CONSTRAINT "PK_864528ec4274360a40f66c29845" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "conversation" ("title" character varying, "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "userId" integer NOT NULL, CONSTRAINT "PK_864528ec4274360a40f66c29845" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "message" ("content" character varying NOT NULL, "role" character varying NOT NULL, "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "conversationId" uuid NOT NULL, CONSTRAINT "PK_ba01f0a3e0123651915008bc578" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "message" ("content" character varying, "fileHash" character varying, "isEncrypted" boolean NOT NULL DEFAULT false, "role" character varying NOT NULL, "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "conversationId" uuid NOT NULL, CONSTRAINT "PK_ba01f0a3e0123651915008bc578" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "conversation" ADD CONSTRAINT "FK_c308b1cd542522bb66430fa860a" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
